9c229aada705736ae2a16f66b18d50c1286e85c7
Jonathan Thompson <Jonathan.Thompson@diamond.ac.uk>
2014-03-14 14:58:04 +0000

    terminalServer: Initial support for ACS and MOXA terminal servers.

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,19 @@
+from iocbuilder import AutoSubstitution
 
+from iocbuilder.modules.snmp import Snmp
 
+class moxa(AutoSubstitution):
+    Dependencies = (Snmp,)
+    TemplateFile = 'Moxa.template'
+
+class moxaChannel(AutoSubstitution):
+    Dependencies = (Snmp,)
+    TemplateFile = 'MoxaChannel.template'
+
+class acs(AutoSubstitution):
+    Dependencies = (Snmp,)
+    TemplateFile = 'Acs.template'
+
+class acsChannel(AutoSubstitution):
+    Dependencies = (Snmp,)
+    TemplateFile = 'AcsChannel.template'
