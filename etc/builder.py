a0fe904d271a0e4b98dbb86f78340b8e3c7d7242
Jonathan Thompson <Jonathan.Thompson@diamond.ac.uk>
2016-10-18 12:28:15 +0000

    Added support for writing to the terminal servers, autosave & archiving.

--- etc/builder.py
+++ etc/builder.py
@@ -2,6 +2,13 @@ from iocbuilder import AutoSubstitution
 from iocbuilder import Device
 from iocbuilder.arginfo import *
 from iocbuilder.modules.snmp import Snmp
+from iocbuilder.modules.calc import Calc
+
+class _terminalServer32(AutoSubstitution):
+    TemplateFile = 'TerminalServer32.template'
+
+class _terminalServer16(AutoSubstitution):
+    TemplateFile = 'TerminalServer16.template'
 
 class _moxa(AutoSubstitution):
     TemplateFile = 'Moxa.template'
@@ -15,32 +22,42 @@ class _acs(AutoSubstitution):
 class _acsChannel(AutoSubstitution):
     TemplateFile = 'AcsChannel.template'
 
-class moxa(Device):
-    Dependencies = (Snmp, )
+class Moxa(Device):
+    Dependencies = (Snmp, Calc)
 
-    def __init__(self, P, R, HOST, NCHANS, **args):
+    def __init__(self, name, P, R, HOST, NCHANS, **args):
         Device.__init__(self)
-        _moxa(P=P, R=R, HOST=HOST, NCHANS=NCHANS)
+        if NCHANS > 16:
+            _terminalServer32(name=name, P=P, R=R)
+        else:
+            _terminalServer16(name=name, P=P, R=R)
+        _moxa(P=P, R=R, HOST=HOST)
         for i in range(NCHANS):
             _moxaChannel(P=P, R=R, CHAN=i+1, HOST=HOST)
 
     ArgInfo = makeArgInfo(__init__,
-        P = Simple("PV prefix", str),
-        R = Simple("PV suffix", str),
-        HOST = Simple("Host IP address", str),
-        NCHANS = Simple("Number of channels", int))
+                          name = Simple("Object name", str),
+                          P = Simple("PV prefix", str),
+                          R = Simple("PV suffix", str),
+                          HOST = Simple("Host IP address", str),
+                          NCHANS = Simple("Number of channels", int))
 
-class acs(Device):
+class Acs(Device):
     Dependencies = (Snmp, )
 
-    def __init__(self, P, R, HOST, NCHANS, **args):
+    def __init__(self, name, P, R, HOST, NCHANS, **args):
         Device.__init__(self)
-        _acs(P=P, R=R, HOST=HOST, NCHANS=NCHANS)
+        if NCHANS > 16:
+            _terminalServer32(name=name, P=P, R=R)
+        else:
+            _terminalServer16(name=name, P=P, R=R)
+        _acs(P=P, R=R, HOST=HOST)
         for i in range(NCHANS):
             _acsChannel(P=P, R=R, CHAN=i+1, HOST=HOST)
 
     ArgInfo = makeArgInfo(__init__,
-        P = Simple("PV prefix", str),
-        R = Simple("PV suffix", str),
-        HOST = Simple("Host IP address", str),
-        NCHANS = Simple("Number of channels", int))
+                          name = Simple("Object name", str),
+                          P = Simple("PV prefix", str),
+                          R = Simple("PV suffix", str),
+                          HOST = Simple("Host IP address", str),
+                          NCHANS = Simple("Number of channels", int))
